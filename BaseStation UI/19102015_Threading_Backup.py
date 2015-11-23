8962e31d53f1f0318acdb4ee3f07557a6931d242
Kishan <kishansachdeva@gmail.com>
2015-11-23 12:13:35 -0800

    changed settings, broadcasting, created new processes

---
+++ BaseStation UI/19102015_Threading_Backup.py
@@ -0,0 +1,375 @@
+import threading
+import multiprocessing
+import tkMessageBox # for messageboxes 
+import Tkinter as tk
+from PIL import ImageTk , Image
+import cv2 # OpenCV for video handling
+import tkFont # for fonts
+from time import strftime,sleep
+from collections import deque
+
+allDronesList=['Othrod','The Great Goblin','Boldog','Ugluk','Bolg','Orcobal','More Orcs','Orc1','Orc2','Orc3','Orc4']
+activeDronesList=['Othrod','Ugluk','Bolg','Orcobal'] 
+# move these lists to the respective buffer /data structures eventually 
+
+class MyUAV(threading.Thread):
+    def __init__(self,master,r,c,rspan,cspan):
+        threading.Thread.__init__(self)
+        MyUAV=tk.Frame(master)
+        MyUAV.grid(row=r, 
+                    column=c,
+                    rowspan=rspan,
+                    columnspan=cspan,
+                    sticky=tk.N+tk.S+tk.W+tk.E)#stretch the widget both horizontally and 
+                                # vertically to fill the cell
+        
+        testButton=tk.Button(master,
+                            text='Overwrite',
+                            command=master.OnButton)
+        testButton.grid(row=r, 
+                        column=c,
+                        rowspan=rspan,
+                        columnspan=cspan,
+                        sticky = tk.N+tk.S+tk.W+tk.E)
+
+    def run(self):
+        i=0
+        while i<10:
+            print 'i is =',i
+            # print '# active threads in MyUAV loop are',threading.enumerate()
+            sleep(20)
+            i+= 1
+
+class otherdrones(threading.Thread):
+    def __init__(self,master):
+        threading.Thread.__init__(self)
+        #otherDroneFrame=tk.Frame(master)
+        otherDroneCanvas = tk.Canvas(master) # to add scroll bar
+        otherDroneCanvas.grid(row=0,
+                        column=2,
+                        rowspan=1,
+                        columnspan=3,
+                        sticky=tk.N+tk.S+tk.W+tk.E)
+        otherDroneCanvas.rowconfigure(0,weight=1)
+        #self.updateActiveDrones(droneFrame)threading.Thread
+        # Intialize places for
+        i=0 # counter for referencing objects in the list
+        self.allDroneDict=dict() # initalizing empty dictionary 
+        for orc in allDronesList:
+            otherDroneCanvas.columnconfigure(i,weight=1)
+            self.allDroneDict[orc]=tk.Button(otherDroneCanvas,text=orc, bg = "gray14", fg="snow")
+            self.allDroneDict[orc].grid(row=0,column=i,sticky=tk.N+tk.S+tk.E+tk.W)
+            i=i+1
+
+        scrollBarOtherDrones = AutoScrollbar(otherDroneCanvas,orient=tk.HORIZONTAL)
+        scrollBarOtherDrones.grid(row=1,columnspan=i,sticky=tk.E+tk.W)
+
+    def run(self):
+        sleep(2) # remove this eventually
+        i=1
+        while 1:
+            self.updateActiveDrones()
+            if (i%60)==0: # print every 30 seconds - thread is alive
+                print "Updated Active Drones in the vicinity" 
+            i=i+1
+            sleep(0.5) # sleep for 500ms before updating
+
+    def updateActiveDrones(self):
+        # add missing key error exceptions here
+        for orc in activeDronesList:
+            self.allDroneDict[orc].configure(bg='medium spring green', fg='black')
+
+class Video(threading.Thread):
+    def __init__(self,master):
+        threading.Thread.__init__(self)
+        self.vidFrame=tk.Frame(master)
+        #stickyelf.vidFrame.config(padx=20)
+ 
+        self.vidFrame.grid(row=1,
+                      column=1,
+                      rowspan=3,
+                      columnspan=3,
+                      sticky=tk.S+tk.N+tk.E+tk.W)
+        self.vidLabel=tk.Label(self.vidFrame)
+
+        # self.vidLabel.grid(row=1,
+        #               column=1,
+        #               rowspan=3,
+        #               columnspan=2,
+        #               sticky=tk.S+tk.N+tk.E+tk.W)
+
+        self.vidLabel.pack(fill=tk.BOTH,expand=1)
+
+        def enforceAspectRatio(event):
+            dw=int(0.7*event.width)
+            dh=int(0.75*event.width)
+            print "w,h is ",event.width, event.height,dw,dh
+            self.vidLabel.config(width=dw,height=dh)
+            print "frame size is", self.vidFrame.winfo_width(), self.vidFrame.winfo_height()  
+
+        #self.vidFrame.bind("<Configure>",enforceAspectRatio)
+        
+
+        # Intialize vidControl Frame
+        vidControl =tk.Frame(master)
+        vidControl.grid(row=1,
+                      column=4,
+                      rowspan=1,
+                      columnspan=1,
+                      sticky=tk.N+tk.S+tk.E+tk.W)
+        self.recordButton = tk.Button(vidControl, 
+                                        text="Record", 
+                                        bd = 1,
+                                        bg= "Red",
+                                        command= self.recordVideo)
+        self.recordButton.pack(fill=tk.BOTH,
+                                expand=1)
+        toggleCameraButton = tk.Button(vidControl, 
+                                            text = "Camera Toggle", 
+                                            command=self.toggleCamera)
+        toggleCameraButton.pack(fill=tk.BOTH,
+                                    expand=1)
+        screenshotButton = tk.Button(vidControl, 
+                                            text = "Screen Capture",
+                                            command=self.screenshot)
+        screenshotButton.pack(fill=tk.BOTH,
+                                   expand=1)
+        depthToggleButton = tk.Button(vidControl,
+                                        text = "Show Depth",
+                                        command=self.depthToggle)
+        depthToggleButton.pack(fill=tk.BOTH,
+                                    expand=1)
+
+    def run(self):
+        self.saveVideoToggle=0 # Intialize videocapture toggle to zero
+        self.takeScreenShot=0 # Intialize screenshot toggle to be zero
+        self.cameraChannelOnVideo=0 # Intialize Camera Channel to default to zero
+        self.vid_cap = cv2.VideoCapture(self.cameraChannelOnVideo) # Assign channel to video capture
+        self.showVideo(self.vidLabel,self.vidFrame)
+
+    def recordVideo(self):
+        try:
+            w=int(self.vid_cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH ))
+            h=int(self.vid_cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT ))
+            fourcc = cv2.cv.CV_FOURCC('D','I','V','X')
+            outputFPS = 20.0
+            self.vidWriter = cv2.VideoWriter('Video_'+str(self.cameraChannelOnVideo)+'_'+strftime("%c")+'.avi', fourcc, outputFPS, (w, h), True)
+            #self.videoWriter.open("output.avi", fourcc, outputFPS, (w, h))
+            self.saveVideoToggle=1 # start capturing video frames in video loop
+            self.recordButton.configure(text="Stop", 
+                                        bg= "black",
+                                        fg ="snow",
+                                        command=self.stopVideoRecord)
+        except:
+            print "Something bad happened with recordVideo"
+
+    def stopVideoRecord(self):
+        self.saveVideoToggle=0
+        print "Stopped Recording Video"
+        self.recordButton.configure(text="Record",
+                                    bg="Red",
+                                    fg ="Black",
+                                    command= self.recordVideo)
+        self.vidWriter.release()
+
+    def depthToggle(self):
+        print 'Depth Toggling function goes here- Read from the stereo camera manual how to do this'
+
+    def screenshot(self):
+        self.takeScreenShot=1
+        print 'Took a Screenshot!'
+
+    def toggleCamera(self):
+        # works for only two cameras
+        #try: # error handling is not working
+        newChannel = 1-self.cameraChannelOnVideo
+        self.vid_cap = cv2.VideoCapture(newChannel)
+        self.cameraChannelOnVideo=newChannel
+
+        isChannelValid,_= self.vid_cap.read(0)
+        if isChannelValid:
+            self.cameraChannelOnVideo=newChannel
+        else:
+            self.cameraChannelOnVideo=0
+            self.vid_cap = cv2.VideoCapture(self.cameraChannelOnVideo) # result to default camera method
+        print 'Displaying Video Feed from Camera Number = ',self.cameraChannelOnVideo 
+        # except:
+        #     print 'Hi'
+        
+    def showVideo(self,vidLabel,vidFrame):
+        _, frame = self.vid_cap.read(0) 
+        frame = cv2.flip(frame, 1) # flips the video feed
+        cv2image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+    
+        # save image if screenshot toggle is on
+        if self.takeScreenShot==1:
+            img.save('Camera '+str(self.cameraChannelOnVideo)+'_'+strftime("%c")+'.jpg')
+            self.takeScreenShot=0
+
+        if self.saveVideoToggle==1:
+           # print "Hi there, Recording Video"
+           #print self.videoWriter.isOpened
+           self.vidWriter.write(frame)
+
+        frameAspectRatio = (float(vidFrame.winfo_width())/float(vidFrame.winfo_height()))
+        if frameAspectRatio > (1.333): # Frame is wider than it needs to be
+           new_height= int(10*(vidFrame.winfo_height()/10)) # round image size to nearest
+           new_width=int(1.33*new_height)
+        else: # Frame is taller than it needs to be
+            new_width= int(10*(vidFrame.winfo_width()/10))
+            new_height =int(0.75* new_width)
+        img_resize= img.resize([new_width,new_height]) #resizing image
+        imgtk = ImageTk.PhotoImage(image=img_resize)
+        vidLabel.imgarbage = imgtk # for python to exclude image from garbage collection
+        vidLabel.configure(image=imgtk)
+        vidLabel.after(2,self.showVideo,vidLabel,vidFrame) # calls the method after 10 ms
+   
+class Application(tk.Frame):              
+    def __init__(self): #,master= None):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.grid(sticky=tk.N+tk.S+tk.E+tk.W) #The argument sticky=tk.N+tk.S+tk.E+tk.aW to self.grid() is necessary so that the Application widget will expand to fill its cell of the top-level window's grid
+        # make top level of the application stretchable and space filling 
+        top=self.winfo_toplevel() 
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        # make all rows and columns grow with the widget window ; weight signifies relative rate of window growth
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, weight=1) 
+
+        Status=tk.Frame(self)
+        Status.grid()
+        #Team=tk.Frame(self)
+        #Team.grid()
+        Settings=tk.Frame(self)
+        Settings.grid()
+        Log=tk.Frame(self)
+        Log.grid()
+
+
+        videoThread=Video(self)
+        otherDrones=otherdrones(self)
+        myUAVThread=MyUAV(self,0,0,1,2)
+
+
+        # Quit even if some operations are remaining to be complete
+
+        videoThread.setDaemon(True) 
+        myUAVThread.setDaemon(True)
+        otherDrones.setDaemon(True)  
+
+        self.createWidgets(Status,'Stats',1,0,2,1)
+        self.createWidgets(Settings,'Settings',3,0,1,1)
+        self.createWidgets(Log,'Logging',2,4,2,1)
+
+
+        print '# active threads are ',threading.enumerate()
+        videoThread.start() # becomes mainthread
+        myUAVThread.start() # becomes secondard thread
+        otherDrones.start()
+        print '# active threads are ',threading.enumerate()
+
+
+    def createWidgets(self,frame,txt,r,c,rspan,cspan):
+        # for testing
+
+        self.qt = tk.Button(self, text=txt,command=self.quit)
+        self.qt.grid(row=r, column=c,rowspan=rspan,columnspan=cspan,
+        sticky=tk.N+tk.S+tk.W+tk.E) #stretch the widget both horizontally and 
+
+    def OnButton(self):
+        # for testing only
+        result=tkMessageBox.askokcancel(title="File already exists", message="File already exists. Overwrite?")
+        if result is True:
+            print "User clicked Ok"
+        else:
+            print "User clicked Cancel"
+
+class AutoScrollbar(tk.Scrollbar):
+    # a scrollbar that hides itself if it's not needed.  only
+    # works if you use the grid geometry manager.
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            # grid_remove is currently missing from Tkinter!
+            self.tk.call("grid", "remove", self)
+        else:
+            self.grid()
+        Scrollbar.set(self, lo, hi)
+    def pack(self, **kw):
+        raise TclError, "cannot use pack with this widget"
+    def place(self, **kw):
+        raise TclError, "cannot use place with this widget"
+        raise tk.TclError, "cannot use place with this widget"
+
+class listener(threading.Thread):
+    def __init__(self,sizeOfBuffer):
+        threading.Thread.__init__(self)
+        self.receviedPacketBuffer = deque([], sizeOfBuffer)
+        print "Initialized Ring Buffer as size of", sizeOfBuffer
+
+    def run(self):
+        for i in xrange(30): # replace by reading head
+            self.receviedPacketBuffer.append(i)
+            print "Buffer is : ", self.receviedPacketBuffer
+            #print "Last element is ", self.receviedPacketBuffer[len(self.receviedPacketBuffer)-1] # show last element - required for other operations
+            sleep(0.1)
+
+class logger(threading.Thread):
+    def __init__(self,listeningThread):
+        threading.Thread.__init__(self)
+        outfile='testfile.txt'
+        self.listenerobject=listeningThread
+        self.log_dummy=open(outfile,"w",1) # use a = append mode, buffering set to true
+        print "file", outfile, "is opened"
+        sleep(0.5) # sleep 1 second while initailizing so that buffer can stream intial values
+
+    def run(self): 
+        try :
+            tempData=""
+            m=0
+
+            while 1:
+                print '# active threads are ',threading.enumerate()
+                if len(self.listenerobject.receviedPacketBuffer)>0:
+                    data=strftime("%c")+"\t"+str(self.listenerobject.receviedPacketBuffer.popleft())+"\n"
+                    print 'writing this now',data # for testing
+                    tempData=tempData+data
+                    m=m+1
+
+                if m%5==0:                
+                    self.log_dummy.write(tempData)
+                    tempData=""
+
+
+        except IndexError:
+            print "No elements in the Buffer"
+            self.log_dummy.close()
+
+def UDP():
+    listeningThread=listener(6) # sizeOfRingBuffer
+    listeningThread.setDaemon(False) # exit UI even if some listening is going on
+    listeningThread.start()
+    #loggingThread.setDaemon(False) # Do not exit if things are pending here
+    loggingThread=logger(listeningThread)
+    loggingThread.setDaemon(False)
+    loggingThread.start()
+    #loggingThread.setDaemon(False) # Do not exit if things are pending here
+
+
+def main():
+    udpProcess=multiprocessing.Process(name='UDP Process', target=UDP)
+    udpProcess.start()    
+    # start tkinter stuff
+    # root = Application()
+    # root.master.title("Azog")
+    # root.mainloop()
+            
+if __name__ == '__main__':
+    main()    
